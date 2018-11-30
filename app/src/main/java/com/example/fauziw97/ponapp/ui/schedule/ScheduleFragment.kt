b5b1c5e4eeaf1fdc5b371d2fa71ef77b460b1a8d
ahmad <ahmadfauziw97@gmail.com>
2018-11-30 19:18:46 +0700

    Started schedule

--- app/src/main/java/com/example/fauziw97/ponapp/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/fauziw97/ponapp/ui/schedule/ScheduleFragment.kt
@@ -1,13 +1,16 @@
 package com.example.fauziw97.ponapp.ui.schedule
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.fauziw97.ponapp.R
+import com.example.fauziw97.ponapp.ui.util.toast
+import kotlinx.android.synthetic.main.fragment_schedule.*
+import org.joda.time.format.DateTimeFormat
 
 
 class ScheduleFragment : Fragment() {
@@ -16,8 +19,18 @@ class ScheduleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_schedule, container, false)
+    }
+
+
+    @SuppressLint("NewApi")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        weekCalendar.setOnDateClickListener {
+            val fmt = DateTimeFormat.forPattern("d MMM yyyy")
+            val str = it.toString(fmt)
+            toast(str, true)
+        }
     }
 
 
